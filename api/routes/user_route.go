8643952db31945f38fb67691a2c0e19b11089d22
Enes Akbal <enesakbal00@gmail.com>
2025-04-04 20:15:11 +0300

    chore:

---
+++ api/routes/user_route.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/enesakbal/todo-api/api/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func NewUserRoute(controllers controllers.UserController, group *gin.RouterGroup) {
+	group.POST("/users", controllers.CreateUser)
+	group.GET("/users", controllers.GetAllUsers)
+}
